2c576fb84d0746daf2fad709d1eca619c1dd44bf
Micah Fitch <micahscopes@gmail.com>
2022-01-26 14:45:59 -0600

    messy mess vfx

--- examples/patch.js
+++ examples/patch.js
@@ -2,7 +2,7 @@ import normals from "angle-normals";
 import { fquad, uvGrid, Alg } from "../src";
 
 import Delaunator from "delaunator";
-import { chunk } from "lodash-es";
+import { chunk, add, zip, unzipWith } from "lodash-es";
 
 import glLib from "../gpu/lib.glsl";
 
@@ -11,22 +11,32 @@ let gl = function (s) {
   ${glLib}\n\n${s[0]}`;
 };
 
-export default draw = (regl, resolution = 64, defaultOffset = [0, 0, 0]) => {
+const normalize = x => {
+    const norm = x.reduce((a,b)=> a+b, 0)
+    return x.map(n => n/norm);
+}
+function addVector(a,b){
+  return a.map((e,i) => e + b[i]);
+}
+
+export default draw = (regl, resolution = 64, defaultOffset = [0, 0, 0], randomness=1, squashFactor=5) => {
   const grid = Delaunator.from(uvGrid(resolution));
   const D = 10;
   const r = (x) => x + Math.random() * 20;
-  const randomUnit = (D=1) => {
-    let x = [Math.random(), Math.random(), Math.random(),];
-    const norm = x.reduce((a,b)=> a+b, 0)
-    return x.map(n => D*n/norm);
-  };
+  const randomUnit = (D=1) =>
+    normalize([Math.random(), Math.random(), Math.random()]).map(x => x*D)
 
-  const period = Math.random()/10
+  const period = Math.random()/30
 
   let patch = {};
   patch.cells = chunk(grid.triangles, 3);
   patch.positions = chunk(grid.coords, 2).map(([u, v]) => [u, v, 0]);
   patch.normals = normals(patch.cells, patch.positions);
+
+  const p0 = addVector(normalize([0,0,randomness*Math.random()/5]).map(x => x*D), randomUnit(randomness*D/10));
+  const p1 = addVector(normalize([1,0,randomness*Math.random()/5]).map(x => x*D), randomUnit(randomness*D/10));
+  const p2 = addVector(normalize([0,1,randomness*Math.random()/5]).map(x => x*D), randomUnit(randomness*D/10));
+  const p3 = addVector(normalize([1,1,randomness*Math.random()/5]).map(x => x*D), randomUnit(randomness*D/10));
 
   return regl({
     vert: gl`
@@ -65,46 +75,50 @@ export default draw = (regl, resolution = 64, defaultOffset = [0, 0, 0]) => {
       }`,
     frag: gl`
       precision highp float;
-      uniform float time;
+      uniform float redAmount;
       varying vec3 n;
       varying vec2 uv;
       void main () {
         vec3 light = vec3(0.5,0.9,0.5);
         light = normalize(light);
 
-        // gl_FragColor = vec4(uv, 0.25+0.5*abs(cos(time/2.0)), 1.0);
-        gl_FragColor = vec4(0, pow(uv.x, 2.0), pow(uv.y, 2.0), 1);
+        gl_FragColor = vec4(redAmount, uv.x, uv.y, 0.2);
       }`,
     attributes: {
       position: () => patch.positions,
       normal: () => patch.normals,
     },
-    uniforms: {
-      // time(){ return Date.now() },
-      // p0: [0, 0, Math.random()*D],
-      // p1: [D, 0, Math.random()*D],
-      // p2: [0, D, Math.random()*D],
-      // p3: [D, D, Math.random()*D],
-      p0: ({tick}) => [D/2,0,0].map(x => x*Math.cos(tick*period)),
-      p1: randomUnit(D),
-      p2: ({tick}) => [0,D/2,0].map(x => x*Math.sin(tick*period)),
-      p3: randomUnit(D),
-      w0: randomUnit(2),
-      w1: randomUnit(2),
-      w2: randomUnit(2),
-      w3: randomUnit(2),
-      offset: (context, props) =>
-        props?.offset ? props.offset.map((x) => x * D) : defaultOffset.map(x => x*D),
+    depth: {
+      enable: false
+    },
 
-      // w00: [1, 0, -1],
-      // w0: ({tick}) => [Math.sin(tick/150), 1, 0],
-      // w10: ({tick}) => [0, 0, 2*Math.sin(tick/150)],
-      // w11: ({tick}) => [0, 2*3.1415926*Math.sin(tick/50), -1],
-      // time: ({tick}) => {
-      //   // console.log(Date.now())
-      //   return tick/50
-      // }
-      // time: 10
+    blend: {
+      enable: true,
+      func: {
+        // srcRGB: 'one minus dst alpha',
+        srcRGB: 1,
+        srcAlpha: 1,
+        dstRGB: 1,
+        dstAlpha: 1
+      },
+      equation: {
+        rgb: 'subtract',
+        alpha: 'add'
+      },
+      color: [0, 0, 0, 0]
+    },
+    uniforms: {
+      p0: ({tick}) => p0.map(x => x*Math.cos(tick*period/4)),
+      p1: ({tick}) => p1.map(x => x*Math.sin(tick*period/3)),
+      p2: ({tick}) => p2.map(x => x*Math.sin(tick*period/5)),
+      p3: ({tick}) => p3.map(x => x*Math.cos(tick*period/7)),
+      w0: randomUnit(D),
+      w1: randomUnit(D),
+      w2: randomUnit(D),
+      w3: randomUnit(D),
+      offset: (context, props) =>
+        props?.offset ? props.offset.map((x) => x * D / squashFactor) : defaultOffset.map(x => x*D/squashFactor),
+      redAmount: ({tick}) => (Math.cos(tick*period)+1)/3
     },
     elements: patch.cells,
     count: patch.cells.length * 3,
