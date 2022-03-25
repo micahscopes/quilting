8b6917779e1ad7950dbfc9762fba8cc1097c0a71
Micah Fitch <micahscopes@gmail.com>
2022-03-25 17:59:43 -0500

    it works!

--- src/lod-patches.ts
+++ src/lod-patches.ts
@@ -16,28 +16,9 @@
  * @return {function: } an interpolation function.
  */
 export const quadEdgeWeightInterpolator =
-  (A: number, B: number, C: number, D: number, n = 4)=>
+  (A: number, B: number, C: number, D: number, n = 4) =>
   ([u, v]: number[]) =>
-    (u ** n * C + (1 - v) ** n * D + (1 - u) ** n * A + v ** n * B) ;
-
-// window.quadInt = quadEdgeWeightInterpolator
-
-// export const quadEdgeWeightInterpolator =
-//   (A: number, B: number, C: number, D: number) => ([u,v]: number[]) => {
-//     // from https://math.stackexchange.com/a/830139/
-//     let coefU = 0.5;
-//     let coefV = 0.5;
-//     const distU = 0.5 - Math.abs(u - 0.5);
-//     const distV = 0.5 - Math.abs(v - 0.5);
-//     if (distU + distV > 0.000000000000000001) {
-//       const distSum = distU + distV;
-//       coefU = distU / distSum;
-//       coefV = distV / distSum;
-//     }
-//     return (
-//       A * (1 - u) * coefU + B * (1-v) * coefV + C * u * coefU + D * u * coefV
-//     );
-//   };
+    u ** n * C + (1 - v) ** n * D + (1 - u) ** n * A + v ** n * B;
 
 /**
  * Generate a function that interpolates between edge weights of a triangle
@@ -48,14 +29,59 @@ export const quadEdgeWeightInterpolator =
  * @return {function: } an interpolation function that takes barycentric coordinates.
  */
 export const triEdgeWeightInterpolator =
-  (A: number, B: number, C: number) =>
-  ([x, y, z]: number[]) =>
-    ((1 - x) * A + (1 - y) * B + (1 - z) * C) / 2;
+  (A: number, B: number, C: number, n = 1) =>
+  ([x, y, z]: number[]) => {
+    // x = 1-x; y=1-y; z=1-z;
+    // float e0 = barys.x * barys.y;
+    // float e1 = barys.y * barys.z;
+    // float e2 = barys.z * barys.x;
+    const e0 = y*z;
+    const e1 = x*z
+    const e2 = x*y
+    // float sum = e0+e1+e2;
+    const sum = e0+e1+e2;
+
+    const c0 = A;
+    const c1 = B;
+    const c2 = C
+
+    return ((e0*c0)**n + (e1*c1)**n + (e2*c2)**n) / sum**n;
+
+    // return (
+    //   ((y * z) ** n * A + (x * z) ** n * B + (x * y) ** n * C) *
+    //   (y * z + x * z + x * y)
+    // );
+  };
 
 import { KdTreeSet } from "@thi.ng/geom-accel";
 import { DensityFunction, samplePoisson } from "@thi.ng/poisson";
 import { randMinMax2 } from "@thi.ng/vectors";
-import { range } from "lodash-es";
+import { range, sum } from "lodash-es";
+import bc from "barycentric-coordinates";
+import barycentric from "barycentric";
+
+const cartesian = (bc, corners) => [
+  bc[0] * corners[0][0] + bc[1] * corners[1][0] + bc[2] * corners[2][0],
+  bc[0] * corners[0][1] + bc[1] * corners[1][1] + bc[2] * corners[2][1],
+];
+
+const {
+  // triangleCartesianCoords,
+  triangleBarycentricCoords,
+} = bc;
+// console.log(bc)
+
+type Point2D = [number, number];
+
+const sampleTriangle = (corners: [Point2D, Point2D, Point2D]) => () => {
+  const u = [Math.random(), Math.random()];
+  const su0 = Math.sqrt(u[0]);
+  const b0 = 1 - su0;
+  const b1 = u[1] * su0;
+
+  const pt = cartesian([b0, b1, 1 - b0 - b1], corners);
+  return pt;
+};
 
 export const quadPatch = (
   resA: number,
@@ -63,10 +89,87 @@ export const quadPatch = (
   resC: number,
   resD: number
 ) => {
-  const getRadius = (pt: number[]) => {
-    const x = quadEdgeWeightInterpolator(resA, resB, resC, resD)(pt);
-    return 1 / x / 2;
+  const triangleFanWeights: [number, number, number][] = [
+    [resA, (resA + resB) / 2, (resA + resD) / 2],
+    [resB, (resB + resC) / 2, (resB + resA) / 2],
+    [resC, (resC + resD) / 2, (resC + resB) / 2],
+    [resD, (resD + resA) / 2, (resD + resC) / 2],
+  ];
+  // const triangleFanWeights: [number, number, number][] = [
+  //   [resA, 2, 2],
+  //   [resB, 2, 2],
+  //   [resC, 2, 2],
+  //   [resD, 2, 2],
+  // ];
+  console.log(triangleFanWeights, resA, resB, resC, resD);
+  const triangleFanCoords: [Point2D, Point2D, Point2D][] = [
+    [
+      [0.5, 0.5],
+      [0, 1],
+      [0, 0],
+    ],
+    [
+      [0.5, 0.5],
+      [0, 0],
+      [1, 0],
+    ],
+    [
+      [0.5, 0.5],
+      [1, 0],
+      [1, 1],
+    ],
+    [
+      [0.5, 0.5],
+      [1, 1],
+      [0, 1],
+    ],
+  ];
+
+  const getRadiusTri =
+    (
+      triangle: [Point2D, Point2D, Point2D],
+      weights: [number, number, number]
+    ) =>
+    (pt: Point2D) => {
+      // const bary = triangleBarycentricCoords(
+      //   [...pt, 1],
+      //   triangle.map((x) => [...x, 1])
+      // );
+      const bary = barycentric(triangle, pt)
+      const x = triEdgeWeightInterpolator(
+        1 / weights[0],
+        1 / weights[1],
+        1 / weights[2],
+        // 0.45
+      )(bary);
+      // console.log(x, bary, pt, triangle, weights)
+      return x;
+    };
+
+  const downward = (x: number) => -(x - 0.5) + 0.5;
+  const upward = (x: number) => x - 0.5 + 0.5;
+
+  const getTriangleInfoForPoint = ([x, y]: number[]) => {
+    let i = 0;
+    const down = downward(x);
+    const up = upward(x);
+    if (y < down && y > up) {
+      i = 0;
+    } else if (y < down && y < up) {
+      i = 1;
+    } else if (y > down && y < up) {
+      i = 2;
+    } else if (y > down && y > up) {
+      i = 3;
+    }
+    return { weights: triangleFanWeights[i], corners: triangleFanCoords[i], i };
   };
+
+  const getRadiusAuto = (pt: Point2D) => {
+    const { corners, weights, i } = getTriangleInfoForPoint(pt);
+    return getRadiusTri(corners, weights)(pt);
+  };
+
   const index = new KdTreeSet(2);
   const addPoint = (radius: number) => (x: number[]) =>
     index.add(
@@ -75,48 +178,52 @@ export const quadPatch = (
     );
 
   // add stitching points
-  // range(0, resA + 1, 1 / resA)
-  //   .map((a) => [0, a])
-  //   .forEach(addPoint(1 / resA));
-  // range(0, resB + 1, 1 / resB)
-  //   .map((b) => [b, 0])
-  //   .forEach(addPoint(1 / resB));
-  // range(0, resC + 1, 1 / resC)
-  //   .map((c) => [1, c])
-  //   .forEach(addPoint(1 / resC));
-  // range(0, resD + 1, 1 / resD)
-  //   .map((d) => [d, 1])
-  //   .forEach(addPoint(1 / resD));
-
-  for (let a = 0; a < 1; a += 1 / resA) {
+  for (let a = 0; a <= 1; a += 1 / resA) {
     index.add([0, a]);
   }
-  for (let b = 0; b < 1; b += 1 / resB) {
-    index.add([b, 1]);
+  for (let b = 0; b <= 1; b += 1 / resB) {
+    index.add([b, 0]);
   }
-  for (let c = 0; c < 1; c += 1 / resC) {
+  for (let c = 0; c <= 1; c += 1 / resC) {
     index.add([1, c]);
   }
-  for (let d = 0; d < 1; d += 1 / resD) {
-    index.add([d, 0]);
+  for (let d = 0; d <= 1; d += 1 / resD) {
+    index.add([d, 1]);
   }
 
+  const sampleTri = (
+    corners: [Point2D, Point2D, Point2D],
+    weights: [number, number, number]
+  ) => {
+    ////////////////////////
+    // const density = getRadiusTri(corners, weights) as DensityFunction
+    // const points = sampleTriangle(corners)
+    ////////////////////////
+    // const points = () => randMinMax2(null, [0, 0], [1, 1])
+    const points = () => [Math.random(), Math.random()];
+    const density = getRadiusAuto as DensityFunction;
+    window.density = density;
+    ////////////////////////
+    return samplePoisson({
+      index,
+      ////////////////////////
+      points,
+      density,
+      ///////////////////////////
+      iter: 2,
+      jitter: 0.00001,
+      max: 5000,
+      quality: 5000,
+    });
+  };
   const boundaryPoints = index.keys();
 
   const points = [
     ...boundaryPoints, //
-    ...samplePoisson({
-      index,
-      points: () => {
-        const pt = randMinMax2(null, [0, 0], [1, 1]);
-        return pt;
-      },
-      density: getRadius as DensityFunction,
-      iter: 5,
-      jitter: 0.001,
-      max: 10000,
-      quality: 1000,
-    }),
+    ...sampleTri(triangleFanCoords[0], triangleFanWeights[0]),
+    // ...sampleTri(triangleFanCoords[1], triangleFanWeights[1]),
+    // ...sampleTri(triangleFanCoords[2], triangleFanWeights[2]),
+    // ...sampleTri(triangleFanCoords[3], triangleFanWeights[3]),
   ];
 
   return { points, index };
