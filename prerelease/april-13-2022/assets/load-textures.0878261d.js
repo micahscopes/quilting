b4904b5f2c6edaaf6ce295e71233725ef550b4c0
Micah Fitch <micahscopes@gmail.com>
2022-04-13 10:07:29 -0500

    new ganja.js tricks for rollup compatibility

---
+++ prerelease/april-13-2022/assets/load-textures.0878261d.js
@@ -0,0 +1,47 @@
+import { _ as __vitePreload } from './index.4bdc8132.js';
+
+var seafoam = "/prerelease/april-13-2022/assets/seafoam.8d324085.png";
+
+var swoosh = "/prerelease/april-13-2022/assets/swoosh.678c5af0.png";
+
+var documentReady = async function () {
+  if (document.readyState === "complete") {
+    return Promise.resolve();
+  }
+  return new Promise(function (resolve) {
+    window.addEventListener("load", resolve);
+  });
+};
+
+const matcapUrls = { "./matcaps/00001.png": () => (true? __vitePreload(()=>import('./00001.1b388197.js'),[]): import('./00001.1b388197.js')), "./matcaps/00002.png": () => (true? __vitePreload(()=>import('./00002.6183821a.js'),[]): import('./00002.6183821a.js')), "./matcaps/00003.png": () => (true? __vitePreload(()=>import('./00003.f8244440.js'),[]): import('./00003.f8244440.js')), "./matcaps/00004.png": () => (true? __vitePreload(()=>import('./00004.c8c92232.js'),[]): import('./00004.c8c92232.js')), "./matcaps/00005.png": () => (true? __vitePreload(()=>import('./00005.9c0a7fb2.js'),[]): import('./00005.9c0a7fb2.js')), "./matcaps/00006.png": () => (true? __vitePreload(()=>import('./00006.6d5df454.js'),[]): import('./00006.6d5df454.js')), "./matcaps/00007.png": () => (true? __vitePreload(()=>import('./00007.43444650.js'),[]): import('./00007.43444650.js')), "./matcaps/00008.png": () => (true? __vitePreload(()=>import('./00008.1c52e4b9.js'),[]): import('./00008.1c52e4b9.js')), "./matcaps/00009.png": () => (true? __vitePreload(()=>import('./00009.c51e5213.js'),[]): import('./00009.c51e5213.js')), "./matcaps/00010.png": () => (true? __vitePreload(()=>import('./00010.5ab1408b.js'),[]): import('./00010.5ab1408b.js')), "./matcaps/00011.png": () => (true? __vitePreload(()=>import('./00011.81854b41.js'),[]): import('./00011.81854b41.js')), "./matcaps/00012.png": () => (true? __vitePreload(()=>import('./00012.ce615638.js'),[]): import('./00012.ce615638.js')), "./matcaps/00013.png": () => (true? __vitePreload(()=>import('./00013.332b3db4.js'),[]): import('./00013.332b3db4.js')), "./matcaps/00014.png": () => (true? __vitePreload(()=>import('./00014.1067e44e.js'),[]): import('./00014.1067e44e.js')), "./matcaps/00015.png": () => (true? __vitePreload(()=>import('./00015.824f3d68.js'),[]): import('./00015.824f3d68.js')), "./matcaps/00016.png": () => (true? __vitePreload(()=>import('./00016.237759a6.js'),[]): import('./00016.237759a6.js')), "./matcaps/00017.png": () => (true? __vitePreload(()=>import('./00017.2fbf8050.js'),[]): import('./00017.2fbf8050.js')), "./matcaps/00018.png": () => (true? __vitePreload(()=>import('./00018.920d049b.js'),[]): import('./00018.920d049b.js')), "./matcaps/00019.png": () => (true? __vitePreload(()=>import('./00019.07cfc5c1.js'),[]): import('./00019.07cfc5c1.js')), "./matcaps/00020.png": () => (true? __vitePreload(()=>import('./00020.3620932d.js'),[]): import('./00020.3620932d.js')), "./matcaps/00021.png": () => (true? __vitePreload(()=>import('./00021.50873d07.js'),[]): import('./00021.50873d07.js')), "./matcaps/00022.png": () => (true? __vitePreload(()=>import('./00022.0fdfc2dd.js'),[]): import('./00022.0fdfc2dd.js')), "./matcaps/00023.png": () => (true? __vitePreload(()=>import('./00023.4e3aa06e.js'),[]): import('./00023.4e3aa06e.js')), "./matcaps/00024.png": () => (true? __vitePreload(()=>import('./00024.012cf919.js'),[]): import('./00024.012cf919.js')), "./matcaps/00025.png": () => (true? __vitePreload(()=>import('./00025.0f47c0f4.js'),[]): import('./00025.0f47c0f4.js')), "./matcaps/00026.png": () => (true? __vitePreload(()=>import('./00026.7f1b0858.js'),[]): import('./00026.7f1b0858.js')), "./matcaps/00027.png": () => (true? __vitePreload(()=>import('./00027.3edff3dd.js'),[]): import('./00027.3edff3dd.js')), "./matcaps/00028.png": () => (true? __vitePreload(()=>import('./00028.a1efebe4.js'),[]): import('./00028.a1efebe4.js')), "./matcaps/00029.png": () => (true? __vitePreload(()=>import('./00029.ace5c63e.js'),[]): import('./00029.ace5c63e.js')), "./matcaps/00030.png": () => (true? __vitePreload(()=>import('./00030.97cdab37.js'),[]): import('./00030.97cdab37.js')), "./matcaps/00031.png": () => (true? __vitePreload(()=>import('./00031.6a356ffb.js'),[]): import('./00031.6a356ffb.js')), "./matcaps/00032.png": () => (true? __vitePreload(()=>import('./00032.2ed705a4.js'),[]): import('./00032.2ed705a4.js')), "./matcaps/00033.png": () => (true? __vitePreload(()=>import('./00033.2dfac6f9.js'),[]): import('./00033.2dfac6f9.js')), "./matcaps/00034.png": () => (true? __vitePreload(()=>import('./00034.4c411ca9.js'),[]): import('./00034.4c411ca9.js')), "./matcaps/00035.png": () => (true? __vitePreload(()=>import('./00035.26cca53d.js'),[]): import('./00035.26cca53d.js')), "./matcaps/00036.png": () => (true? __vitePreload(()=>import('./00036.c4a4ec0e.js'),[]): import('./00036.c4a4ec0e.js')), "./matcaps/00037.png": () => (true? __vitePreload(()=>import('./00037.d66bbfaa.js'),[]): import('./00037.d66bbfaa.js')), "./matcaps/00038.png": () => (true? __vitePreload(()=>import('./00038.a2b92e62.js'),[]): import('./00038.a2b92e62.js')), "./matcaps/00039.png": () => (true? __vitePreload(()=>import('./00039.7be80548.js'),[]): import('./00039.7be80548.js')), "./matcaps/00040.png": () => (true? __vitePreload(()=>import('./00040.19239bb8.js'),[]): import('./00040.19239bb8.js')), "./matcaps/00041.png": () => (true? __vitePreload(()=>import('./00041.7badeb3a.js'),[]): import('./00041.7badeb3a.js')), "./matcaps/00042.png": () => (true? __vitePreload(()=>import('./00042.1e61820f.js'),[]): import('./00042.1e61820f.js')), "./matcaps/00043.png": () => (true? __vitePreload(()=>import('./00043.2596141b.js'),[]): import('./00043.2596141b.js')), "./matcaps/00044.png": () => (true? __vitePreload(()=>import('./00044.0085c379.js'),[]): import('./00044.0085c379.js')), "./matcaps/00045.png": () => (true? __vitePreload(()=>import('./00045.0a7f5ca3.js'),[]): import('./00045.0a7f5ca3.js')), "./matcaps/00046.png": () => (true? __vitePreload(()=>import('./00046.a816a741.js'),[]): import('./00046.a816a741.js')), "./matcaps/00047.png": () => (true? __vitePreload(()=>import('./00047.f58b7c42.js'),[]): import('./00047.f58b7c42.js')), "./matcaps/00048.png": () => (true? __vitePreload(()=>import('./00048.c8cdf107.js'),[]): import('./00048.c8cdf107.js')), "./matcaps/00049.png": () => (true? __vitePreload(()=>import('./00049.677de5eb.js'),[]): import('./00049.677de5eb.js')),};
+
+const seafoamImage = await new Promise(async (resolve) => {
+  await documentReady();
+  const im = new Image();
+  im.crossOrigin = "anonymous";
+  im.src = seafoam;
+  im.onload = () => resolve(Object.assign(im, { style: "max-width:200px" }));
+});
+const swooshImage = await new Promise(async (resolve) => {
+  await documentReady();
+  const im = new Image();
+  im.crossOrigin = "anonymous";
+  im.src = swoosh;
+  im.onload = () => resolve(Object.assign(im, { style: "max-width:200px" }));
+});
+const matcapImages = await Promise.all(
+  Object.values(matcapUrls).map(
+    (urlModule) =>
+      new Promise(async (resolve) => {
+        await documentReady();
+        const im = new Image();
+        im.crossOrigin = "anonymous";
+        const url = (await urlModule()).default;
+        im.src = url;
+        im.onload = () =>
+          resolve(Object.assign(im, { style: "max-width:200px" }));
+      })
+  )
+);
+
+export { matcapImages, seafoamImage, swooshImage };
